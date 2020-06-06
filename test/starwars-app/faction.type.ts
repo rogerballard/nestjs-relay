10bc1a81a8970d8cc221863b783e4222473401ba
Roger Ballard <roger@park-it-solutions.com>
2020-06-06 16:29:21 +0100

    refactor(starwars app): Remove unused imports

--- test/starwars-app/faction.type.ts
+++ test/starwars-app/faction.type.ts
@@ -1,6 +1,5 @@
 import { ObjectType, Field } from '@nestjs/graphql'
 import { GlobalID, Node } from '../../src/nestjs-relay-tools'
-import { Ship, returnsShips } from './ship.type'
 
 @ObjectType({ implements: [Node] })
 export class Faction implements Node {
