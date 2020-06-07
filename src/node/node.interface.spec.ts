0aff6273380239dbdbbf7df577912ef4bf080122
Roger Ballard <roger@park-it-solutions.com>
2020-06-07 14:52:25 +0100

    test(node interface): Add tests for node interface helpers

--- src/node/node.interface.spec.ts
+++ src/node/node.interface.spec.ts
@@ -1,14 +1,24 @@
-import { returnsNode, typeNode, Node } from './node.interface'
+import { returnsNode, typeNode, Node, typeNodes, returnsNodes } from './node.interface'
 
 describe('NodeInterface', () => {
   describe('typeNode', () => {
     it('should return the Node class', () => {
-      expect(typeNode()).toBe(Node)
+      expect(typeNode()).toEqual(Node)
+    })
+  })
+  describe('typeNodes', () => {
+    it('should return an array of the Node class', () => {
+      expect(typeNodes()).toEqual([Node])
     })
   })
   describe('returnsNode', () => {
     it('should return the Node class', () => {
-      expect(returnsNode()).toBe(Node)
+      expect(returnsNode()).toEqual(Node)
+    })
+  })
+  describe('returnsNodes', () => {
+    it('should return an array of the Node class', () => {
+      expect(returnsNodes()).toEqual([Node])
     })
   })
 })
