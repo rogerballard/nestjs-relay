9ea01820476393ce0a04a08c6b00f0de4b0ac47e
Roger Ballard <roger@park-it-solutions.com>
2020-06-08 22:58:10 +0100

    test(starwars app): Add introduceShip mutation

--- test/starwars-app/ship.resolver.ts
+++ test/starwars-app/ship.resolver.ts
@@ -1,6 +1,14 @@
-import { Resolver } from '@nestjs/graphql'
+import { Resolver, Mutation } from '@nestjs/graphql'
 import { GlobalIdFieldResolver } from '../../src/nestjs-relay-tools'
 import { Ship } from './ship.type'
 
 @Resolver(Ship)
-export class ShipResolver extends GlobalIdFieldResolver(Ship) {}
+export class ShipResolver extends GlobalIdFieldResolver(Ship) {
+  @Mutation(() => Ship, {
+    name: 'introduceShip',
+    nullable: true
+  })
+  introduceShip() {
+    return null
+  }
+}
