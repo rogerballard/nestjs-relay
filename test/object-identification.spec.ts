5b902e5e632e2d82215b406befc927b357fd3cf4
Roger Ballard <roger@park-it-solutions.com>
2020-06-17 19:37:24 +0100

    test: fix supertest imports

--- test/object-identification.spec.ts
+++ test/object-identification.spec.ts
@@ -1,6 +1,6 @@
 import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { StarWarsModule } from './starwars-app';
 
 describe('Object Identification', () => {
