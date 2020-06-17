3ac9710ce731bfceb0ca39668dcf3bb28bb28d9f
Roger Ballard <roger@park-it-solutions.com>
2020-06-17 19:05:43 +0100

    Reconfigure rollup

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,49 +1,40 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
-import camelCase from 'lodash.camelcase'
-import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import pkg from './package.json';
 
-const pkg = require('./package.json')
-
-const libraryName = 'nestjs-relay'
-
-export default {
-  input: `src/${libraryName}.ts`,
-  output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true }
-  ],
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [
-    'apollo-server-express',
-    '@nestjs/common',
-    '@nestjs/core',
-    '@nestjs/graphql',
-    '@nestjs/testing',
-    'graphql-relay',
-    'graphql-tools',
-    'graphql',
-    'reflect-metadata',
-    'supertest'
-  ],
-  watch: {
-    include: 'src/**'
+export default [
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: 'src/nestjs-relay.ts',
+    output: {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true
+    },
+    // output: [
+    //   { file: pkg.main, format: 'cjs', dir: 'dist' },
+    //   { file: pkg.module, format: 'es', dir: 'dist' },
+    // ],
+    external: [
+      'apollo-server-express',
+      '@nestjs/common',
+      '@nestjs/core',
+      '@nestjs/graphql',
+      '@nestjs/testing',
+      'graphql-relay',
+      'graphql-tools',
+      'graphql',
+      'reflect-metadata',
+      'supertest'
+    ],
+    plugins: [
+      typescript()
+    ],
   },
-  plugins: [
-    // Allow json resolution
-    json(),
-    // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true, exclude: ['*.d.ts', '**/*.d.ts'] }),
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
-
-    // Resolve source maps to the original source
-    sourceMaps()
-  ]
-}
+];
