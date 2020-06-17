f5b0bd4ae095306f3ae597aa126efbbbdd1f05bc
Roger Ballard <roger@park-it-solutions.com>
2020-06-17 20:58:19 +0100

    ci(jest config): move jest config to separate file

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  collectCoverage: true,
+  coveragePathIgnorePatterns: ['/node_modules/', '/test/'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 95,
+      lines: 95,
+      statements: 95
+    }
+  },
+  moduleFileExtensions: ['ts', 'js'],
+  roots: ['src', 'test'],
+  testEnvironment: 'node',
+  testRegex: '.spec.ts$',
+  transform: { '.ts': 'ts-jest' }
+}
