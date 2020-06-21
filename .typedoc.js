7be18eca43771f9a3d77b44021d6398ebafdbc51
Roger Ballard <roger@park-it-solutions.com>
2020-06-21 12:16:14 +0100

    docs(api): set name and version in api docs

--- .typedoc.js
+++ .typedoc.js
@@ -1,9 +1,12 @@
 module.exports = {
-  entryPoint: '"nestjs-relay"',
+  // entryPoint: '"nestjs-relay"',
   exclude: '**/*+(index|.spec|.e2e).ts',
   excludeExternals: true,
   excludeNotExported: true,
+  includeVersion: true,
   inputFiles: ['src'],
+  mode: 'modules',
+  name: 'NestJS Relay',
   out: 'public',
   plugin: 'typedoc-neo-theme',
   theme: './node_modules/typedoc-neo-theme/bin/default',
